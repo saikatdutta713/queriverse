60c48bd24590e65635ba47cfdfaae0d15b2798db
saikatdutta713 <saikatdutta713@gmail.com>
2024-04-23 19:22:45 +0530

    Create Post Activity Created

--- app/src/main/java/com/example/queriverse/Newpost.java
+++ app/src/main/java/com/example/queriverse/Newpost.java
@@ -1,8 +1,6 @@
 package com.example.queriverse;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Splash extends AppCompatActivity {
-
-    private static final int SPLASH_LENGTH = 3000;
+public class Newpost extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_newpost);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(Splash.this,Onboarding.class);
-                startActivity(intent);
-                finish();
-            }
-        },SPLASH_LENGTH);
     }
 }
